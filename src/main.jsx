ced33c3db8702fc569085d193170efc2371cbe00
satyam kumar <satyamjsr69@gmail.com>
2026-04-23 14:03:21 +0530

    Production Changes

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/ShoppingCart'>
       <Provider store={store}>
         <App />
       </Provider>
